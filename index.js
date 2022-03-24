a3e57d72f840bec5723375313c6caa94e31756d3
Mimsical <therealvibezyt@gmail.com>
2022-03-23 20:59:14 -0400

    Switched to old proxy.

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-console.log("Server-side code running");
-
-const express = require("express");
-const path = require("path");
-
-
-const app = express();
-
-const port = process.env.PORT || 3000;
-
-// sendFile will go here
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/index.html"));
-});
-
-app.listen(port);
-console.log("Server started at http://localhost:" + port);
+(async() => {
+  await import('./index.mjs');
+})();
